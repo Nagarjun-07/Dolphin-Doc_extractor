fca42d5a490316a7b644166a6f3e1561a18280db
Nagarjun H bharadwaj <119668048+Nagarjun-07@users.noreply.github.com>
2025-07-29 23:35:57 +0530

    Update dev.ts

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,6 +1,5 @@
 import { config } from 'dotenv';
 config();
-
 import '@/ai/flows/extract-formulas-from-document.ts';
 import '@/ai/flows/extract-text-from-document.ts';
 import '@/ai/flows/extract-tables-from-document.ts';
